9260518cdf62a8846aa52fd595c8bd09161c15fb
Kevin Ploch <kevin.ploch@tum.de>
2021-05-11 12:50:32 +0200

    wrote tests for parse_gossip_announce

--- testing/test_packet_parser.py
+++ testing/test_packet_parser.py
@@ -5,14 +5,40 @@ from struct import pack
 
 
 class Test_header_funcs(unittest.TestCase):
-    def test_get_size(self):
-        test_packet1 = pack(pp.FORMAT_GOSSIP_ANNOUNCE+"H", 10, 500, 0, 0, 0, 0)
-        self.assertEqual(pp.get_header_size(test_packet1),
-                         10, "Size should be 10")
+    def test_parse_gossip_announce(self):
+        # Note: We test for correct parsing, wo do not test whether the
+        #       field values make sense.
+
+        # correct packet
+        test_packet = pack(pp.FORMAT_GOSSIP_ANNOUNCE+"H", 10, 500, 0, 0, 0, 0)
+        self.assertEqual(pp.parse_gossip_announce(test_packet),
+                         (10, 500, 0, 0, 0, b'\x00\x00'),
+                         "Gossip Announce parsed incorrectly")
+
+        # correct packet: correct size, empty data
+        test_packet = pack(pp.FORMAT_GOSSIP_ANNOUNCE, 8, 500, 0, 0, 0)
+        self.assertEqual(pp.parse_gossip_announce(test_packet),
+                         (8, 500, 0, 0, 0, b''),
+                         "Empty data was not parsed correctly")
+
+        # wrong packet: empty buffer (b'')
+        test_packet = b''
+        self.assertEqual(pp.parse_gossip_announce(test_packet),
+                         (), "Empty byte (b'') should return empty tuple")
+
+        # wrong packet: correct size, packet too small
+        test_packet = pack("!HHBB", 6, 500, 0, 0)
+        self.assertEqual(pp.parse_gossip_announce(test_packet),
+                         (), "Packet should throw error, too small to parse")
+
+        # wrong packet: incorrect size
+        test_packet = pack(pp.FORMAT_GOSSIP_ANNOUNCE+"H", 80, 500, 0, 0, 0, 0)
+        self.assertEqual(pp.parse_gossip_announce(test_packet),
+                         (), "Packet should have a wrong size")
 
     def test_get_type(self):
-        test_packet1 = pack(pp.FORMAT_GOSSIP_ANNOUNCE+"H", 10, 500, 0, 0, 0, 0)
-        self.assertEqual(pp.get_header_type(test_packet1),
+        test_packet = pack(pp.FORMAT_GOSSIP_ANNOUNCE+"H", 10, 500, 0, 0, 0, 0)
+        self.assertEqual(pp.get_header_type(test_packet),
                          500, "Message type should be 500 - Gossip Announce")
 
 
