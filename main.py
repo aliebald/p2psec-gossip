f49aa507c3d34c8165807ff6dc56d0975e268f76
Alexander Liebald <alexander.liebald@gmail.com>
2021-05-22 15:39:30 +0200

    added test configs and testmode

--- main.py
+++ main.py
@@ -1,14 +1,38 @@
+from threading import Thread
 from modules.config import Config
 from modules.gossip import Gossip
 
 
-# TODO Test Function that starts multiple peers with different configs
+# Test Function that starts multiple peers with different configs
+def test_mode():
+    paths = ["./test_configs/config_1.ini",
+             "./test_configs/config_2.ini",
+             "./test_configs/config_3.ini"]
+
+    for path in paths:
+        print("Try to start", path)
+        try:
+            Thread(target=start_gossip, args=(path, )).start()
+        except:
+            print("Error: unable to start thread for {}".format(path))
+
+
+# Reads the config from the given path and starts Gossip
+def start_gossip(path):
+    print("Starting gossip. Path: \"{}\"".format(path))
+    config = Config(path)
+    config.print_config()
+    Gossip(config)
+
 
 def main():
     # TODO read path from command line
-    config = Config("./config.ini")
-    config.print_config()
-    gossip = Gossip(config)
+    testmode = True
+
+    if testmode:
+        test_mode()
+    else:
+        start_gossip("./config.ini")
 
 
 if __name__ == "__main__":
