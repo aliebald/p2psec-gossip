ee1b3cff8f47c98e1ce59a024a3388f520df1cac
Kevin Ploch <kevin.ploch@tum.de>
2021-06-22 17:41:31 +0200

    created api_connection and implemented first draft

---
+++ modules/api/api_connection.py
@@ -0,0 +1,134 @@
+"""
+This Module provides the Api_connection class
+"""
+from modules.packet_parser import (
+        GOSSIP_ANNOUNCE,
+        GOSSIP_NOTIFY,
+        GOSSIP_VALIDATION,
+        get_header_type,
+        parse_gossip_announce,
+        parse_gossip_notify,
+        parse_gossip_validation
+)
+
+
+class Api_connection:
+    """An Api_connection represents a connection to a single API user.
+
+    Class variables:
+    - gossip (Gossip) -- gossip responsible for this peer
+    - reader (StreamReader) -- (private) asyncio StreamReader of connected peer
+    - writer (StreamWriter) -- (private) asyncio StreamWriter of connected peer
+    - api_listening_port (int) -- listening_port of connected API
+    """
+
+    def __init__(self, reader, writer, gossip):
+        """
+        Arguments:
+        - reader (StreamReader) -- asyncio StreamReader of connected peer
+        - writer (StreamWriter) -- asyncio StreamWriter of connected peer
+        - gossip (Gossip) -- gossip responsible for this peer
+        """
+        self.gossip = gossip
+        self.__reader = reader
+        self.__writer = writer
+
+    async def run(self):
+        """Waits for incoming messages and handles them. Runs until the
+        connection is closed"""
+        while True:
+            try:
+                # TODO: correct?
+                size = await self.__reader.read(2)
+                buf = size + await self.__reader.read(size-2)
+                await self.__reader.read()  # read trash/too long packet
+            except ConnectionError:
+                await self.gossip.close_api(self)
+                return
+            await self.__handle_incoming_message(buf)
+
+    async def close(self):
+        """Closes the connection to the API user.
+        Gossip.close_api() should be called preferably, since it also removes
+        the API from the API list and datatype dictionary."""
+        print("Connection to {} closed".format(self.get_debug_address()))
+        try:
+            self.__writer.close()
+            await self.__writer.wait_closed()
+        except:
+            return
+
+    def get_api_address(self):
+        """Returns the address of this API user in the format host:port
+
+        See also:
+        - get_own_address()
+        - get_api_listening_address()
+        - get_debug_address() - for debug output
+        """
+        # TODO returns None on error
+        (host, port) = self.__writer.get_extra_info('peername')
+        address = "{}:{}".format(host, port)
+        return address
+
+    def get_own_address(self):
+        """Returns the address of this API user in the format host:port
+
+        See also:
+        - get_api_address()
+        - get_api_listening_address()
+        - get_debug_address() - for debug output
+        """
+        (host, port) = self.__writer.get_extra_info('sockname')
+        address = "{}:{}".format(host, port)
+        return address
+
+    def get_debug_address(self):
+        """Returns the name/address for debug output.
+        Only use for debug output!
+
+        Returns:
+            If we know the api_listening_port, we will return the api
+            listening address of the connected API user, otherwise ist
+            address (with the random port instead of the p2p port and an added
+            *)
+        """
+        addr = self.get_api_listening_address()
+        if addr is None:
+            addr = self.get_api_address() + "*"
+        return addr
+
+    async def __handle_gossip_announce(self, buf):
+        return
+
+    async def __handle_gossip_notify(self, buf):
+        """adds the API user to our Subscriber dictionary
+        """
+        (datatype) = parse_gossip_notify(buf)
+        self.gossip.add_subscriber(datatype, self)
+        return
+
+    async def __handle_gossip_validation(self, buf):
+        return
+
+    async def __handle_incoming_message(self, buf):
+        """Checks the type of an incoming message in byte format and calls the
+        correct handler according the the type.
+
+        Arguments:
+        - buf (byte-object) -- received message
+        """
+        type = get_header_type(buf)
+        if type == GOSSIP_ANNOUNCE:
+            print("\r\nReceived GOSSIP_ANNOUNCE from",
+                  self.get_debug_address())
+            await self.__handle_gossip_announce(buf)
+        elif type == GOSSIP_NOTIFY:
+            print("\r\nReceived GOSSIP_NOTIFY from", self.get_debug_address())
+            await self.__handle_gossip_notify(buf)
+        elif type == GOSSIP_VALIDATION:
+            print("Received GOSSIP_VALIDATION from", self.get_debug_address())
+            self.__handle_gossip_validation(buf)
+        else:
+            print("\r\nReceived message with unknown type {} from {}".format(
+                type, self.get_debug_address()))
