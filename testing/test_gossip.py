2a15acb28f65093ca7074826f3c27759520fc815
Kevin Ploch <kevin.ploch@tum.de>
2021-08-27 16:52:18 +0200

    started blackbox testing of Gossip class

--- testing/test_gossip.py
+++ testing/test_gossip.py
@@ -1,5 +1,11 @@
-"""This test subscribes with 2 APIs to the datatype 1 and validates with both
-after the PEER_ANNOUNCE"""
+"""Black Box Testing of the central Gossip class.
+HOWTO:
+    1. Run the main.py
+    2. Run this program
+
+We send messages and await a certain correct answer.
+Otherwise the test fails.
+"""
 
 import socket
 import struct
@@ -11,31 +17,45 @@ GOSSIP_VALIDATION = 503
 FORMAT_GOSSIP_NOTIFY = "!HHHH"
 FORMAT_GOSSIP_VALIDATION = "!HHHH"
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as peer:
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as api1:
-        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as api2:
-            # Connect with both apis
-            api1.connect(('127.0.0.1', 7001))
-            api2.connect(('127.0.0.1', 7001))
-            # Subscribe to Datatype 1
-            api1.sendall(struct.pack(FORMAT_GOSSIP_NOTIFY, 8, GOSSIP_NOTIFY,
-                                     0, 1))
-            api2.sendall(struct.pack(FORMAT_GOSSIP_NOTIFY, 8, GOSSIP_NOTIFY,
-                                     0, 1))
 
-            # Connect as peer
-            peer.connect(('127.0.0.1', 6001))
-            # Send PEER_ANNOUNCE
-            peer.sendall(pp.pack_peer_announce(1, 2, 1, b''))
+def main():
+    test_peer_announce()
+    test_gossip_announce()
 
-            sleep(5)
 
-            # Send GOSSIP_VALIDATION
-            api1.sendall(struct.pack(FORMAT_GOSSIP_VALIDATION, 8,
-                                     GOSSIP_VALIDATION, 1, 1))
-            api2.sendall(struct.pack(FORMAT_GOSSIP_VALIDATION, 8,
-                                     GOSSIP_VALIDATION, 1, 1))
+def test_peer_announce():
+    """This test subscribes with 2 APIs to the datatype 1 and validates with
+    both after the PEER_ANNOUNCE"""
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as peer:
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as api1:
+            with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as api2:
+                # Connect with both apis
+                api1.connect(('127.0.0.1', 7001))
+                api2.connect(('127.0.0.1', 7001))
+                # Subscribe to Datatype 1
+                api1.sendall(struct.pack(FORMAT_GOSSIP_NOTIFY, 8,
+                                         GOSSIP_NOTIFY, 0, 1))
+                api2.sendall(struct.pack(FORMAT_GOSSIP_NOTIFY, 8,
+                                         GOSSIP_NOTIFY, 0, 1))
+                # Connect as peer
+                peer.connect(('127.0.0.1', 6001))
+                # Send PEER_ANNOUNCE
+                peer.sendall(pp.pack_peer_announce(1, 2, 1, b''))
 
-            # Idle
-            while True:
-                sleep(1)
+                # Check for received GOSSIP_NOTIFICATION
+
+                # Send GOSSIP_VALIDATION
+                api1.sendall(struct.pack(FORMAT_GOSSIP_VALIDATION, 8,
+                                         GOSSIP_VALIDATION, 1, 1))
+                api2.sendall(struct.pack(FORMAT_GOSSIP_VALIDATION, 8,
+                                         GOSSIP_VALIDATION, 1, 1))
+                # Idle
+                while True:
+                    sleep(1)
+
+
+def test_gossip_announce(): pass
+
+
+if __name__ == "__main__":
+    main()
