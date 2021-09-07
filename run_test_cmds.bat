04d413704ad855bb830f18be549b679f93196e90
Alexander Liebald <alexander.liebald@tum.de>
2021-09-07 21:25:23 +0200

    Adjusted run_test_cmds-bat for new verbose flag and changed message on start

--- run_test_cmds.bat
+++ run_test_cmds.bat
@@ -1,18 +1,18 @@
 :: This script starts multiple instances of gossip with different configs.
 :: Each instance gets its own terminal. 
 
-echo "This terminal will be closed after all test instances are opened."
+echo "Starting 4 delayed instances of Gossip in new terminals and exiting afterwards"
 
-start cmd.exe /k main.py -p "./test_configs/config_1.ini"
-
-:: wait for 8 seconds
-timeout 8 > nul
-start cmd.exe /k main.py -p "./test_configs/config_2.ini"
+start cmd.exe /k main.py -p "./test_configs/config_1.ini" -v
 
 :: wait for 8 seconds
 timeout 8 > nul
-start cmd.exe /k main.py -p "./test_configs/config_3.ini"
+start cmd.exe /k main.py -p "./test_configs/config_2.ini" -v
 
 :: wait for 8 seconds
 timeout 8 > nul
-start cmd.exe /k main.py -p "./test_configs/config_4.ini"
+start cmd.exe /k main.py -p "./test_configs/config_3.ini" -v
+
+:: wait for 8 seconds
+timeout 8 > nul
+start cmd.exe /k main.py -p "./test_configs/config_4.ini" -v
